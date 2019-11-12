8400e56061876992934b43c5f88a091c5bceda49
law-ashman <30593054+law-ashman@users.noreply.github.com>
2019-11-12 18:34:30 +0000

    Create index.php
    Build the initial honepage

---
+++ index.php
@@ -0,0 +1,390 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 9]><html class="ie"><![endif]-->
+<!--[if !IE]><!--><html><!--<![endif]-->
+	<head>
+		<meta charset="UTF-8">
+		<title>Web Consultant 4 Hire - Law Ashman</title>
+
+        <!-- Viewport meta tag to prevent iPhone from scaling our page -->
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<link rel="stylesheet" href="./css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="./css/style.css"/>
+		
+        <!-- Normalize hide address bar for iOS and Android -->
+		<script src="./js/hideaddressbar.js"></script>
+
+        <!-- Add media query support for IE8 and under. Must place media queries in external stylesheet -->
+        <script src="./js/respond.min.js"></script>
+
+        <!-- Using picturefill to load conditional images and matchmedia to provide support for IE8 and below + older browsers -->
+		<script src="js/picturefill.js"></script>
+		<script src="js/matchmedia.js"></script>
+        
+        <!--[if lt IE 9]>
+			<script src="./js/html5.js"></script>
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
+		<![endif]-->
+
+        <!--  Checkbox Hack Support for IE8 above and below -->
+        <!--[if IE 8]>
+        <script src="./js/checkbox.js"></script>
+        <![endif]-->
+
+        <!--  Attribute Selector Support for IE6 -->
+        <!--[if IE 6]>
+        <script src="./js/selectivizr-min.js"></script>
+        <![endif]-->
+
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      
+	</head>
+
+	<body id="home" lang="en">
+		<!--Add opening <section> tag below here-->
+		<div class="container-nav">
+			    <section id="nav" class="group wrapper">
+			        <a class="replace logo" title="Web Consultant 4 Hire" href="#">Web Consultant 4 Hire</a>
+                    <!-- Checkbox Hack markup -->
+                    <input type="checkbox" id="toggle" />
+                    <label for="toggle" onclick>Menu</label>
+                    <!-- /Checkbox Hack markup -->
+			        <nav role="navigation">
+				        <ul>
+                            <li><a href="#resume">Resume</a></li>
+                            <li><a href="#portfolio">Portfolio</a></li>
+                            <li><a href="mailto:l-ellipse@yahoo.co.uk?subject=Portfolio enquiry">Contact</a></li>
+                        </ul>
+			        </nav>
+                  
+		    </section>
+        </div>
+		<div class="container-header">		
+		    <header id="masthead" role="banner" class="wrapper">	
+			    <h1>Law Ashman</h1>
+			    <h2>A creative eye for UI design &amp; development</h2>
+		    </header>
+        </div>
+		<div class="container-content">    
+		    <article id="content" role="contentinfo">
+                    <section id="main-text" class="wrapper">
+                        <h3>Welcome</h3>
+                        <p>Law Ashman is a Web Consultant 4 hire, available for new assignments from April 2019. This is a portfolio website showcasing web development and design.</p>
+                        <p>For specific examples of user interface design and user experience, download the <a href="docs/WC4H_UI_UX_examples.pdf" title="design profile PDF">design profile</a>.</p>
+                    </section>
+                
+                    <section id="resume" class="group">
+				    	
+			          <header class="wrapper">
+					    <h3>Resume</h3>
+					    <p>Current/previous positions and skills</p>
+			          </header>
+                      <article class="wrapper">
+				        <div class="box">
+				            <h4>Last project</h4>
+
+				            <h5>Content Designer/Producer</h5>
+
+				            <h6>Freelance work</h6>
+
+				            <p>Designed PDF and web promotional flyers.<br />
+                            Produced video promos for Facebook and Instagram. <br />
+                            Designed infographics and inspirational quotes.</p>
+			                </div> 
+                      </article>
+                      <article class="wrapper"> 
+                        <div class="box">
+				        <h4>Previous project</h4>
+                            
+                            <h5>UI Developer/Designer</h5>
+
+				            <h6>Northdoor Plc</h6>
+
+				            <p>Rebranded a legacy insurance product for AIG.<br />
+                            Updated user journeys inline with new brand guidelines and provided mock-ups.<br />
+                            Worked in a SVN environment, updated CSS, Jquery and C# code.</p>
+			               
+			            </div>
+                    </article>
+                    <article class="wrapper">
+                        <div class="box">
+				            <h4>Skills</h4>
+
+				            <ul>
+					            <li>Creative Cloud</li>
+					            <li>SASS, LESS &amp; HTML5</li>
+                                <li>Javascript libraries (Mootools &amp; JQuery)</li>                    
+                                <li>Server side integration (C#, PHP &amp; JSP)</li>
+					            <li>Responsive Web Design Frameworks</li>
+					            <li>Accessibility Awareness</li>
+				            </ul>
+				        </div>
+                    </article>
+			            
+			
+			</section>
+
+                    <!-- Classes removed / #portfolio -->
+			        <!-- Element(s)/Classes added from Bootstrap's Scaffolding -->
+			 
+				    <section id="portfolio" class="container-fluid">
+				   
+				        <header>	
+					        <h3>Portfolio</h3>
+					        <p>Projects and assignments completed</p>
+			            </header>
+                        
+                         <div class="row">
+                             <article class="col-md-4">
+				                <h4>Energie Fitness Enfield</h4>
+                                <p>Social media promo</p>
+						            <a href="https://www.instagram.com/p/Bx0fBaoHNS9/?utm_source=ig_web_copy_link">
+					                    <figure>	
+						                    <img src="./img/promo-thumb.jpg" alt="Hatha Yoga Promo">	
+					                    </figure>
+                                    </a>   
+					        </article>
+                             
+                            <article class="col-md-4">
+				                <h4>Energie Fitness Enfield</h4>
+                                <p>Poster design</p>
+						            <a href="./docs/hatha-vinyasa-yoga_flyer-FINAL.pdf">
+					                    <figure>	
+						                    <img src="./img/yogaposter-thumb.jpg" alt="Energie Fitness - Yoga poster">	
+					                    </figure>
+                                    </a>   
+					        </article>
+                             
+                            <article class="col-md-4">
+				                <h4>Freelance work</h4>
+                                <p>Funeral service booklet</p>
+						            <a href="./docs/Booklet_final.pdf">
+					                    <figure>	
+						                    <img src="./img/booklet-thumb.jpg" alt="Funeral service booklet">	
+					                    </figure>
+                                    </a>   
+                             </article> 
+                             
+                         </div>
+                        
+                        
+                         <div class="row">
+                             
+                             <article class="col-md-4">
+                                  <h4>Gucci</h4>
+                                <p>E-commerce website redesign</p>
+						            <a href="https://www.gucci.com/uk/en_gb/">
+					                    <figure>	
+						                    <img src="./img/gucci-thumb.jpg" alt="Gucci website">	
+					                    </figure>
+                                    </a>
+				                   
+					        </article> 
+                             
+                            <article class="col-md-4">
+                                <h4>HFEA</h4>
+                                <p>Accessibility implementation</p>
+						            <a href="https://www.hfea.gov.uk/">
+					                    <figure>	
+						                    <img src="./img/hfea-thumb.jpg" alt="HFEA website">	
+					                    </figure>
+                                    </a>
+					        </article>
+             
+                            <article class="col-md-4">
+                                 <h4>Currencies Direct Partners</h4>
+                                <p>Wireframes of a CRM portal</p>
+						            <a href="http://www.currenciesdirect.com/uitest/partners/portal/">
+					                    <figure>	
+						                    <img src="./img/portal-thumb.jpg" alt="Currencies Direct Partners Portal wireframes">	
+					                    </figure>
+                                    </a>
+					        </article>
+                             
+                             
+                         </div> 
+                        
+                        
+                         <!-- row 2 -->
+
+                        <div class="row">
+                            
+                             <article class="col-md-4">
+				                <h4>Currencies Direct FX app</h4>
+                                <p>Design of an new app section</p>
+
+					            <a href="https://online.currenciesdirect.com/CustomerPortal/">
+					                <figure>	
+						                <img src="./img/fxplatform-thumb.jpg" alt="Currencies Direct FX platform">	
+					                </figure> 
+                                 </a>
+					        </article> 
+                            
+                            <article class="col-md-4">
+				                <h4>Online Sellers Calculator</h4>
+                                <p>Design of a pay per click campaign</p>
+
+					            <a href="http://www.currenciesdirect.com/ui/ppc/e-tailers/etailers-collection-account.php">
+					                <figure>	
+						                <img src="./img/ppc3-thumb.jpg" alt="Online Sellers Calculator PPC">	
+					                </figure>
+                                 </a>
+					        </article>
+                            
+                              <article class="col-md-4">
+				                <h4>Online Sellers </h4>
+                                <p>Implementation of a microsite</p>
+
+					            <a href="http://www.currenciesdirect.com/uitest/online-sellers/">
+					                <figure>	
+						                <img src="./img/os-thumb.jpg">	
+					                </figure>
+                                 </a>
+					        </article>
+                         </div> 
+                         
+                         <!-- row 3 -->
+                         
+                         <div class="row">
+                             
+                             <article class="col-md-4">
+				                <h4>Everything Overseas guide</h4>
+                                <p>Design of a pay per click campaign</p>
+						            <a href="http://everythingoverseas.com/property-guides/south-africa.php">
+					                    <figure>	
+						                    <img src="./img/eo1-thumb.jpg" alt="Everything Overseas pay per click campaign">	
+					                    </figure>
+                                    </a>   
+					        </article> 
+                             
+                            <article class="col-md-4">
+				                <h4>Evidence Hub</h4>
+                                <p>Design of templates for a portal</p>
+						            <a href="https://evidencehub.islington.gov.uk/" title="Islington Evidence Hub">
+					                    <figure>	
+						                    <img src="./img/eh-thumb.png" alt="Islington Evidence Hub">	
+					                    </figure>
+                                    </a>   
+					        </article>
+                             
+                            <article class="col-md-4">
+				                <h4>Everything Overseas email </h4>
+                                <p>Design of an email campaign template</p>
+
+					            <a href="./emails/guide-template.html">
+					                <figure>	
+						                <img src="./img/eo2-thumb.jpg">	
+					                </figure>
+                                 </a>
+					        </article> 
+ 
+                         </div>
+                        
+                       
+                         
+                         <div class="row">
+                             
+                               <article class="col-md-4">
+				                <h4>Roxy Palace</h4>
+                                <p>Wireframes for a gaming app</p>
+						            <a href="./wireframes/index.html" title="Gaming mobile website wireframes ">
+					                    <figure>	
+						                    <img src="./img/gaming-thumb.png" alt="Gaming mobile website wireframes">	
+					                    </figure>
+                                    </a>   
+                             </article>
+                             
+                             <article class="col-md-4"> 
+				                <h4>betboo Turkey</h4>
+                                <p>Implementation of a new website launch</p>
+						           <a href="http://tr.betboo.com" title="betboo Turkey website">
+					                <figure>	
+						                <img src="./img/betboo-thumb.jpg" alt="betboo Turkey website">	
+					                </figure>
+                                    </a>
+                            </article>
+                             <article class="col-md-4">
+				                <h4>Sportingbet</h4>
+                                <p>Implementation of new app design</p>
+
+					            <a href="http://fr.sportingbet.com" title="Sportingbet France website">
+					                <figure>	
+						                <img src="./img/sportingbet-thumb.jpg">	
+					                </figure>
+                                 </a>
+					        </article>
+                             
+                               <?php /*
+                               
+                               <h4>Etailers FX app</h4>
+                                <p>Implementation of a new section</p>
+
+					            <a href="https://www.currenciesdirect.com/uitest/etailers/?s=dashboard&p=dashboard">
+					                <figure>	
+						                <img src="./img/etailersfx-thumb.jpg" alt="Etailers FX app">	
+					                </figure>
+                                 </a>
+                               
+                             <article class="col-md-4">
+				                <h4>ParentPort</h4>
+                                <p>Design/implementation of CMS templates</p>
+						            <a href="http://www.parentport.org.uk/" title="ParentPort website">
+					                    <figure>	
+						                    <img src="./img/parentport-thumb.png" alt="ParentPort website">	
+					                    </figure>
+                                    </a>   
+					        </article>   
+                             <article class="col-md-4"> 
+				                <h4>TPR</h4>
+                                <p>Design and implementation of CMS templates </p>
+						           <a href="http://www.towergateprofessionalrisks.co.uk/complementary-therapists.aspx" title="Towergate Professional Risks">
+					                <figure>	
+						                <img src="./img/tpr-thumb.png" alt="Towergate Professional Risks website">	
+					                </figure>
+                                    </a>
+                            </article> 
+                             
+                             <article class="col-md-4">
+				                <h4>Wates</h4>
+                                <p>Assisted with a new CMS website launch</p>
+
+					            <a href="http://www.cbelearning.com" title="Wates website">
+					                <figure>	
+						                <img src="./img/wates-thumb.png" alt="Wates website">	
+					                </figure>
+                                 </a>
+					        </article> */?>
+                             
+                             
+                        </div>  
+				   </section>
+			</article>	
+		</div>
+		<div class="container-footer" >
+		    <footer id="colophon" class="wrapper">
+              <?php /*  <form id="contact-form" class="group" role="form" action="">
+                            <fieldset>
+                               
+                                <legend>Contact</legend>
+                                    <div class="name">
+                                        <label for="name">Name</label>
+                                        <input type="text" id="name" name="name" autofocus required />
+                                    </div>
+                                    <div class="email">
+                                        <label for="email">Email</label>
+                                        <input type="email" id="email" name="email" required />                                    
+                                    </div>
+                                    <div class="message">
+                                        <label for="message">Message</label>
+                                        <textarea id="message" name="message" required></textarea>
+                                    </div>
+                                    <input class="send" type="submit" name="submit" value="Send" />
+                            
+                            </fieldset>
+                        
+            </form> */ ?>
+		        <p class="copyright">© Web Consultant 4 Hire limited, all rights reserved.</p>
+		    </footer>	
+		</div>
+	</body>
+</html>
