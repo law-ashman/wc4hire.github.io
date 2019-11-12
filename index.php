ef07d47df883682eb56ead1a1f465306b9b6fd22
law-ashman <30593054+law-ashman@users.noreply.github.com>
2019-11-12 18:45:12 +0000

    Update index.php
    Updated links to CSS files

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
         <!-- Viewport meta tag to prevent iPhone from scaling our page -->
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-		<link rel="stylesheet" href="./css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="./css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/style.css"/>
 		
         <!-- Normalize hide address bar for iOS and Android -->
 		<script src="./js/hideaddressbar.js"></script>
